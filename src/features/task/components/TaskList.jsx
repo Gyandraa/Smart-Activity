d24a9e5103efc3ae615582992504d14a878fe31b
gyandra <gyandranaupal14@gmail.com>
2026-02-18 14:20:30 +0700

    delete folder shared

--- src/features/task/components/TaskList.jsx
+++ src/features/task/components/TaskList.jsx
@@ -1,3 +1,4 @@
+import Button from "../../../shared/components/button";
 export default function TaskList({ tasks, removeTask, startEdit }) {
   return (
     <ul className="mt-5">
@@ -6,14 +7,6 @@ export default function TaskList({ tasks, removeTask, startEdit }) {
           <div className="text-center">
             <h3>Task: {task.task}</h3>
             <p>Description: {task.description}</p>
-            <button
-              onClick={() => removeTask(task.id)}
-              className="px-4 py-2 ml-5 bg-green-600 text-white font-semibold
-      rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2
-      focus:ring-green-500 focus:ring-opacity-75"
-            >
-              Delete
-            </button>
             <button
               onClick={() => startEdit(task.id)}
               className="px-4 py-2 ml-5 bg-red-600 text-white font-semibold
